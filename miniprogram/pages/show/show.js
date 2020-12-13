3b4028ce648191e38ab7fc4fd1e6d772e59690b7
ROL <785614493@qq.com>
2020-12-13 20:23:45 +0800

    echarts显示正常

--- miniprogram/pages/show/show.js
+++ miniprogram/pages/show/show.js
@@ -1,80 +1,106 @@
 import * as echarts from '../../ec-canvas/echarts';
-//const app = getApp();
-let chart = null;
-function initChart (canvas, width, height, dpr) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
-  var option = {
+const db = wx.cloud.database()
+function setOption(chart, xdata, ydata) {
+  const option = {
     title: {
-      text: '定位销磨损情况',
-      left: 'center'
+      text: '定位销直径点检记录',
+      padding: [10, 0, 0, 20],
+      textStyle: {
+        fontSize: 14,
+        color: '#696969'
+      },
+      top: '10rpx'
     },
-    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
-    legend: {
-      data: ['A', 'B', 'C'],
-      top: 20,
-      left: 'center',
-      //backgroundColor: 'red',
-      z: 100
-    },
+    backgroundColor: "#fff",
+    color: ["#006EFF", "#67E0E3", "#9FE6B8"],
+    animation: false,
     grid: {
-      show: 'true',
-      height: 'auto',
-      width: 'auto',
-      containLabel: true
-    },
-    tooltip: {
-      show: true,
-      trigger: 'axis'
+      show: false
     },
     xAxis: {
       type: 'category',
-      boundaryGap: true,
-      data: ['周一', '周二', '周三', '周四', '周五'],
-      // show: false
+      data: xdata,      //x轴上的数据是动态的，所以我作为参数传进来
     },
     yAxis: {
+      x: 'center',
+      scale: true,
       type: 'value',
-      boundaryGap: ['20%', '20%'],
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      }
-      // show: false
     },
     series: [{
-      name: 'A',
-      type: 'bar',
-      //smooth: true,
-      data: [18, 36, 65, 30, 78]
+      type: 'line',
+      smooth: 0.2,
+      data: ydata,    //y轴上的数据也是动态的，也作为参数传进来
     }]
   };
-
-  chart.setOption(option);
-  return chart;
+  chart.setOption(option)
 }
 
 Page({
-  onShareAppMessage: function (res) {
-
-  },
   data: {
-    ec: {
-      onInit:initChart
-    }
+    ecOne: {
+      lazyLoad: true
+    },
+    ecTwo: {
+      lazyLoad: true
+    },
   },
-  
-  
-
-  onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      // console.log(chart)
-    }, 2000);
+  onLoad: function (options) {
+    this.oneComponent = this.selectComponent('#mychart-one');
+    this.twoComponent = this.selectComponent('#mychart-two');
+  },
+  onReady: function () {               //这一步是一定要注意的
+    this.getOneOption();
+    this.getTwoOption();
+  },
+  init_one: function (xdata, ydata) {
+    //初始化第一个图表
+    this.oneComponent.init((canvas, width, height) => {
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      setOption(chart, xdata, ydata)
+      this.chart = chart;
+      return chart;
+    });
+  },
+  init_two: function (xdata, ydata) {        //初始化第二个图表
+    this.twoComponent.init((canvas, width, height) => {
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      setOption(chart, xdata, ydata)
+      this.chart = chart;
+      return chart;
+    });
+  },
+  getOneOption: function () {
+    let _this = this
+    //这一步其实就要给图表加上数据
+    var xdate = [];
+    var result = [];
+    db.collection('fixture_01')
+      .where({
+        position: 'CP00036901',
+      })
+      .field({
+        pinData: true,
+        time: true,
+      })
+      .limit(10)
+      .get({
+        success: res => {
+          for (var i = 0; i < res.data.length; i++) {
+            xdate.push(res.data[i].time.substr(5, 5))
+            result.push(res.data[i].pinData[0].dm * 1)
+          }
+          _this.init_one(xdate, result)
+        }
+      });
+  },
+  //第二个图表也是一样的处理
+  getTwoOption: function () {
+    this.init_two(['1', '3', '5', '7', '9'], [2, 4, 6, 8, 10])
   }
-});
+})
